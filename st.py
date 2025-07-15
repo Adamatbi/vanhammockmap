0331f0e083b5db084e30ca8ae5b04aad7a420f4c
Adam Atbi <adamatbi@gmail.com>
2025-07-15 13:33:13 -0700

    description

--- st.py
+++ st.py
@@ -3,4 +3,8 @@ import pandas as pd
 
 df = pd.read_csv("good_tree_coords.csv")
 
+st.title("Vancouver Hammock Map")
+st.write("This map shows the locations of tree pairs suitable for hammocks in Vancouver, Canada.")
+st.write("Data Source: https://opendata.vancouver.ca/explore/dataset/public-trees")
+
 st.map(df,size=1)
