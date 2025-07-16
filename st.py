72e2c32a2c5a5fff20bfe4594c726230c99c482b
Adam Atbi <adamatbi@gmail.com>
2025-07-16 13:42:01 -0700

    darkmode and clean text

--- st.py
+++ st.py
@@ -3,8 +3,13 @@ import pandas as pd
 
 df = pd.read_csv("good_tree_coords.csv")
 
+st.set_page_config(
+    page_title="Van Hammock Map",
+)
+
 st.title("Vancouver Hammock Map")
-st.write("This map shows the locations of tree pairs suitable for hammocks in Vancouver, Canada.")
-st.write("Data Source: https://opendata.vancouver.ca/explore/dataset/public-trees")
+st.write("The map shows the locations of tree pairs suitable for hanging a hammock in Vancouver, Canada.")
 
 st.map(df,size=1)
+
+st.write("Data Source: https://opendata.vancouver.ca/explore/dataset/public-trees")
